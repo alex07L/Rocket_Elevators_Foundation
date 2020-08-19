018f04f6e1ab5e048d21054d91ad891cda8eda36
alex07L <alexlevesque7@hotmail.fr>
2020-08-18 23:05:27 -0400

    add context

--- spec/Elevator_Media_spec.rb
+++ spec/Elevator_Media_spec.rb
@@ -2,9 +2,20 @@ require 'ElevatorMedia'
 
 describe ElevatorMedia do
 	describe "building" do
+		streamer = ElevatorMedia::Streamer.new("07112")
+		context "test to get Streamer class" do
+			it "return Streamer class" do
+				expect(streamer).to be_a(ElevatorMedia::Streamer)
+			end
+		end
+		context "test to get http request" do
+			it "return JSON" do
+				expect(streamer.call).to be_a(Hash)
+			end
+		end
 		context "test to get building details" do
 			it "return string" do
-				expect(ElevatorMedia::Streamer.getContent).to eq("<div></div>")
+				expect(streamer.getContent).to eq("<div></div>")
 			end
 		end
 	end
